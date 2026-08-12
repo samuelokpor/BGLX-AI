51208f010825a45ecaed30899cf3189fba8f0450
Samuel Okpor <samuelita6@gmail.com>
2026-08-12 15:49:22 +0800

    Install custom Nav2 behavior tree configuration

--- bglx_ws/src/bglx_navigation/setup.py
+++ bglx_ws/src/bglx_navigation/setup.py
@@ -16,6 +16,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'config', 'bt'), glob('config/bt/*.xml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
